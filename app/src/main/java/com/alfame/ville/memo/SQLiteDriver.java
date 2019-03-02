55fc734b48641570e0a54bb8c8cb25848f745456
anttipls <47867007+anttipls@users.noreply.github.com>
2019-03-02 18:57:03 +0200

    last uppy
    did lots of things

--- app/src/main/java/com/alfame/ville/memo/SQLiteDriver.java
+++ app/src/main/java/com/alfame/ville/memo/SQLiteDriver.java
@@ -12,10 +12,9 @@ import java.util.ArrayList;
 
 import static android.database.sqlite.SQLiteDatabase.openOrCreateDatabase;
 
-//Home of database code, if methods need to be called from MainActivity add them to the interfaces like these methods
 public class SQLiteDriver implements IStorageDriver {
 
-    private String dbName,tableName,idName,titleName,textName,categoryName,deadlineName,struckName;
+    private String tableName,idName,titleName,textName,categoryName,deadlineName,struckName;
     private File file;
     private int idCount;
 
@@ -24,8 +23,6 @@ public class SQLiteDriver implements IStorageDriver {
 
     SQLiteDriver(File file){
         this.file=file.getAbsoluteFile();
-
-        dbName="notes";
         idCount=0;
         tableName="notes";
         idName="ID";
@@ -34,52 +31,48 @@ public class SQLiteDriver implements IStorageDriver {
         categoryName="Category";
         deadlineName="Deadline";
         struckName="Struck";
-
-        System.out.println("Constructor "+ file.getName()+" " +file.getAbsolutePath());
         createDatabase();
 
     }
 
     @Override
     public ArrayList loadItemsList() {
+        //loads Notes from database
         _createDatabase();
         ArrayList<Note> noteList = new ArrayList<>();
         String sql;
         System.out.println("loadItemsList()");
 
-        //UH OH HARDCODED
-        sql = "SELECT ID, TITLE, TEXT, CATEGORY, STRUCK FROM notes ORDER BY ID";
+
+        sql = "SELECT "+idName+", "+titleName+", "+textName+", "+categoryName+", "+deadlineName+", "
+                +struckName+" FROM "+tableName+" ORDER BY ID;";
         Cursor cl = database.rawQuery(sql,null);
         cl.moveToFirst();
         while(!cl.isAfterLast()){
             //checks if struck
-            Boolean struckB=false;
-            if(!cl.getString(4).toUpperCase().equals("FALSE")){
-                struckB=true;
+            Boolean struckB=true;
+            if(!cl.getString(5).toUpperCase().contains("TRUE")){
+                struckB=false;
             }
 
-            //testing
-            //System.out.println("loadItemsList :");
-            //System.out.println("note ID: "+cl.getInt(0)+ " Title: "+cl.getString(1)+" Text: "+cl.getString(2));
-
-            //id,title,text,category,struck
-            Note newNote = new Note(cl.getInt(0),
-                    cl.getString(1),
-                    cl.getString(2),
-                    cl.getString(3));
+            //id,title,text,category,deadline,struck
+            Note newNote = new Note(
+                    cl.getInt(0),//id
+                    cl.getString(1),//title
+                    cl.getString(2),//text
+                    cl.getString(3),//category
+                    cl.getString(4));//deadline
             newNote.setStruck(struckB);
             noteList.add(newNote);
-
             cl.moveToNext();
         }
         cl.close();
+        database.close();
 
-        idCount=noteList.size()+1;
         //prints out all, for testing
         for(int i=0;i<noteList.size();i++){
             noteList.get(i).printNote();
         }
-        database.close();
         return noteList;
     }
 
@@ -103,7 +96,7 @@ public class SQLiteDriver implements IStorageDriver {
                     "'"+ titleName+"', " +
                     "'"+ textName+"', " +
                     "'"+ categoryName+"', " +
-                    // "'"+deadlineName+"', " +
+                    "'"+deadlineName+"', " +
                     "'"+ struckName+"') " +
 
                     "VALUES ("
@@ -111,7 +104,7 @@ public class SQLiteDriver implements IStorageDriver {
                     "'"+note.getTitle()+"'," +
                     "'"+note.getText()+"'," +
                     "'"+note.getCategory()+"'," +
-                    // "'"+note.getDeadlineString()+"'," +
+                    "'"+note.getDeadlineString()+"'," +
                     "'"+String.valueOf(note.isStruck()).toUpperCase()+"'"+
                     ");";
             System.out.println("IDCOUNT: "+idCount+" SQL CODE: "+sql);
@@ -129,6 +122,7 @@ public class SQLiteDriver implements IStorageDriver {
 
     @Override
     public void editItem(Note note) {
+        //updated edited Note in database
         _createDatabase();
 
         /**
@@ -138,16 +132,17 @@ public class SQLiteDriver implements IStorageDriver {
          * Text='note.getText()',
          * Category='note.getCategory()',
          * Deadline='note.getDeadlineString()',
-         * Struck='String.valueOf(note.isStruck()).toUppercase()',
+         * Struck='String.valueOf(note.isStruck()).toUppercase()'
          * WHERE id=note.getID();
          */
+
         try{
             String sql ="UPDATE "+tableName+" SET "+
                     titleName+"='"+note.getTitle()+"', "+
                     textName+"='"+note.getText()+"', "+
                     categoryName+"='"+note.getCategory()+"', "+
                     struckName+"='"+String.valueOf(note.isStruck()).toUpperCase()+"', "+
-                    //deadlineName+"='"+note.getDeadlineString()+"', "+
+                    deadlineName+"='"+note.getDeadlineString()+"', "+
                     categoryName+"='"+note.getCategory()+"' "+
 
                     " WHERE "+idName+"="+note.getID()+";";
@@ -165,6 +160,7 @@ public class SQLiteDriver implements IStorageDriver {
 
     @Override
     public void removeItem(Note note) {
+        //deletes Note from database
         _createDatabase();
         try{
             String sql = "DELETE FROM "+ tableName +" WHERE "+idName+"= "+note.getID()+";";
@@ -180,9 +176,8 @@ public class SQLiteDriver implements IStorageDriver {
 
     @Override
     public void removeAll() {
+        //deletes all Notes from database
         _createDatabase();
-
-        //DELETE FROM or DELETE * FROM, dunno
         try{
             String sql = "DELETE FROM "+tableName+ ";";
             database.execSQL(sql);
@@ -206,23 +201,12 @@ public class SQLiteDriver implements IStorageDriver {
                 System.out.println("createDatabase says file does not exist");
                 _createDatabase();
                 createTables();
-                databaseTesty(1); //add
-                databaseTesty(2); //read
-                _setidCounter();
-
-
-
             }
             else{
-                System.out.println("createDatabase file exists");
+                System.out.println("createDatabase says file exists");
                 _createDatabase();
-                _setidCounter();
-                //databaseTesty(1);
-                //databaseTesty(2);
-
-                //handle it already
-
             }
+            database.close();
 
         }
         catch (Exception e){
@@ -230,16 +214,13 @@ public class SQLiteDriver implements IStorageDriver {
         }
 
 
+
     }
 
     private void _setidCounter() {
+        //fetches ids from database and sets idCount ahead of highest
+        //should always be the last ID of the table
         _createDatabase();
-
-        /*
-
-         */
-
-
         idCount=0;
         String sql = "SELECT ID FROM notes ORDER BY ID;";
         Cursor cl = database.rawQuery(sql,null);
@@ -252,72 +233,11 @@ public class SQLiteDriver implements IStorageDriver {
             cl.moveToNext();
         }
         cl.close();
-        //ArrayList<Note> arrayList = loadItemsList();
-
-        database.close();
-
-
-    }
-
-
-    private void databaseTesty(int selection) {//for testing
-        _createDatabase();
-        /** Testy
-         1 for testing write
-         2 for reading
-         :D
-         **/
-        String sql;
-
-        Cursor cl;
-        switch (selection){
-            case 1:
-                //WRITING
-                Note newNote = new Note(idCount,"NJOICE","I FEEL IT and THINGS","Anttis kaka");
-                addItem(newNote);
-                Note newNote1 = new Note(idCount,"Wwenks","wih wuah wauau ankan kanka","Anttis kaka3");
-                addItem(newNote1);
-                Note newNote2 = new Note(idCount,"Hi","How are yeeet","Anttis kaka1");
-                addItem(newNote2);
-                break;
-
-            case 2:
-
-                //RETRIEVE
-                sql = "SELECT ID, TITLE, TEXT, CATEGORY FROM notes ORDER BY ID;";
-                cl = database.rawQuery(sql,null);
-                cl.moveToFirst();
-                while(!cl.isAfterLast()){
-                    System.out.println("note ID: "+cl.getInt(0)+ " Title: "+cl.getString(1)+" Text: "+cl.getString(2)+" Category: "+cl.getString(3));
-                    cl.moveToNext();
-                }
-                cl.close();
-                break;
-
-            default:
-                break;
-
-        }
         database.close();
     }
 
     private void createTables() {
-
-        //dropping table for safety/testing
-        /*
-         */
-        try{
-            StringBuilder sql = new StringBuilder();
-            sql.append("DROP TABLE notes;");
-
-            database.execSQL(sql.toString());
-            System.out.println("createTables: dropped table");
-        }
-        catch (SQLiteException e){
-            System.out.println("createTable dropping e: "+e);
-        }
-
-
+        //creates the notes table
         try{
             StringBuilder sql = new StringBuilder();
 
@@ -327,7 +247,7 @@ public class SQLiteDriver implements IStorageDriver {
             sql.append(textName  +" VARCHAR(255),");
             sql.append(categoryName     +" VARCHAR(255),");
             sql.append(deadlineName+" VARCHAR(15),");
-            sql.append(struckName   +" BOOLEAN );");
+            sql.append(struckName   +" VARCHAR(10));");
             database.execSQL(sql.toString());
             System.out.println("createTable SQL CODE: "+sql);
         }
@@ -340,18 +260,14 @@ public class SQLiteDriver implements IStorageDriver {
     }
 
     private void _createDatabase() {
-
-        if(database==null){
-            //System.out.println("DATABASE CREATION");
-        }
+        //opens or creates database for app, closed in methods after SQL
         database= openOrCreateDatabase(file.getAbsolutePath(), null,null);
     }
 
 
     public int getidCount(){
-        _createDatabase();
-        _setidCounter();
-        database.close();
+        //gets a clear id for a new note
+        _setidCounter();//closes database as well
         return  idCount;
     }
 }
